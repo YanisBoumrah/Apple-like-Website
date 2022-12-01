e31b7207b452887ebe5dd3c7a9ad3da5c0f88d89
YanisBoumrah <yanis.boumrah@gmail.com>
2022-12-01 17:51:21 +0100

    refactoring the css

--- src/modules/Prix/prixModule.js
+++ src/modules/Prix/prixModule.js
@@ -1,18 +1,23 @@
 import { Environment, OrbitControls, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import React, { Suspense, useContext, useEffect, useRef } from "react";
+import React, { Suspense, useContext, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { Model3 } from "../../components/Scene3";
 import { ColorContext } from "../../context/colorContext";
-
+import { Container,PrixContainer,Iphone,Couleurs,Couleur,Details,Titre,SousTitre,BtnContainer,Btn,BtnLink,Degree,P } from "./styledPrix";
 
 const PrixModule = () => {
 
   const { materials } = useGLTF("/scene.gltf");
 
   const priceRef = useRef(null);
-  
+  const [showForm, setShowForm] = useState(false);
   const { currentColor, changeColorContext } = useContext(ColorContext);
+
+  const handleShowForm = () => {
+    setShowForm(!showForm);
+  };
+
   
   useEffect(() => {
     priceRef.current.style.backgroundColor = `rgba(${currentColor.rgbColor},0.4)`;
@@ -31,8 +36,16 @@ const PrixModule = () => {
   return (
     <Container>
     <PrixContainer ref={priceRef}>
+    <Degree> 360&deg; &#x27F2;</Degree>
       <Iphone>
-      <Degree> 360&deg; &#x27F2;</Degree>
+      <Couleurs>
+          <Couleur color="#505F4E" onClick={() => updateColor("#505F4E", "Alpine Green", "80, 95, 78")} />
+          <Couleur color="#F9E5C9" onClick={() => updateColor("#F9E5C9", "Gold", "249, 229, 201")} />
+          <Couleur color="#574f6f" onClick={() => updateColor("#574f6f", "Deep Purple", "87, 79, 111")} />
+          <Couleur color="#A50011" onClick={() => updateColor("#A50011", "Red", "165, 0, 17")} />
+          <Couleur color="#215E7C" onClick={() => updateColor("#215E7C", "Blue", "33, 94, 124")} />
+        </Couleurs>
+
         <Canvas camera={{ fov: 14 }}>
           <ambientLight intensity={1} />
           <directionalLight intensity={0.4} />
@@ -43,135 +56,26 @@ const PrixModule = () => {
           <Environment preset="sunset" />
           <OrbitControls enableZoom={false} />
         </Canvas>
-        <Couleurs>
-          <Couleur color="#505F4E" onClick={() => updateColor("#505F4E", "Alpine Green", "80, 95, 78")} />
-          <Couleur color="#F9E5C9" onClick={() => updateColor("#F9E5C9", "Gold", "249, 229, 201")} />
-          <Couleur color="#574f6f" onClick={() => updateColor("#574f6f", "Deep Purple", "87, 79, 111")} />
-          <Couleur color="#A50011" onClick={() => updateColor("#A50011", "Red", "165, 0, 17")} />
-          <Couleur color="#215E7C" onClick={() => updateColor("#215E7C", "Blue", "33, 94, 124")} />
-        </Couleurs>
+        
       </Iphone>
-      <Details>
+      <Details >
           <SousTitre>iPhone</SousTitre>
           <Titre>13 Pro Max</Titre>
           <SousTitre>For <P>1 Da</P> Only</SousTitre>
           <BtnContainer>
-            <Btn>Buy</Btn>
+            <Btn onClick={()=>handleShowForm()}>Buy</Btn>
             <BtnLink href="#">Learn More &#x2192;</BtnLink>
           </BtnContainer>
         </Details>
     </PrixContainer>
+    {/* {showForm ? 
+    <div>
+      <span>Hello world</span>
+    </div>:null}
+    <Button onClick={()=>handleShowForm()}>Zebbi</Button> */}
     </Container>
   );
 };
 
 export default PrixModule;
 
-const Container = styled.div`
- width: 100vw;
-  height: 100vh;
-  position: relative;
-  z-index: 1;
-  background-color: var(--white);
-  overflow: hidden;
-
-`;
-const PrixContainer = styled.div`
- width: 100vw;
-  height: 100vh;
-  position: relative;
-  z-index: 1;
-  background-color: "#9BB5CE";
-`;
-
-const Iphone = styled.div`
-  width: 100%;
-  height: 70%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-  cursor: grab;
-`;
-const Couleurs = styled.ul`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  left: 35%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  @media screen and (max-width: 64em) {
-    left: 10%;
-  }
-`;
-const Couleur = styled.li`
-  list-style: none;
-  width: 1.5rem;
-  height: 1.5rem;
-  cursor: pointer;
-  border-radius: 50%;
-  background-color: ${(props) => props.color};
-  margin: 0.5rem 0;
-  border: 1px solid var(--dark);
-`;
-const Details = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Titre = styled.h2`
-  font-size: var(--fontxl);
-  padding: 0.3rem;
-`;
-
-const SousTitre = styled.h2`
-  font-size: var(--fontmd);
-  font-family: var(--fontR);
-`;
-
-const BtnContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding-top: 1rem;
-`;
-
-const Btn = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 0;
-  padding: 0.4rem 1rem;
-  border-radius: 50px;
-  border: none;
-  outline: none;
-  background-color: var(--blue);
-  color: var(--white);
-  cursor: pointer;
-  &:hover {
-    opacity: 0.7;
-  }
-`;
-const BtnLink = styled.a`
-  color: var(--blue);
-  text-decoration: none;
-  margin-left: 1.5rem;
-  &:hover {
-    text-decoration: underline;
-  }
-`;
-
-const Degree = styled.div`
-  font-size: var(--fontsm);
-  font-family: Georgia, 'Times New Roman', Times, serif;
-  position: absolute;
-  top: 2rem;
-`;
-const P = styled.span`
-color:red;`
---
+++ src/modules/Prix/styledPrix.js
@@ -0,0 +1,124 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+ width: 100%;
+  height: 100vh;
+  position: relative;
+  z-index: 1;
+  background-color: var(--white);
+  overflow: hidden;
+
+`;
+// export const Button=styled.button`
+// position: absolute;
+// z-index:2;
+// bottom: 12px;
+// `
+export const PrixContainer = styled.div`
+ width: 100vw;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  z-index: 1;
+  background-color: "red";
+
+`;
+
+export const Iphone = styled.div`
+  width: 50%;
+  height: 70%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  position: relative;
+  background-color: red;
+
+`;
+
+export const Couleurs = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+ 
+  background-color: blue;
+  z-index: 2;
+
+  @media screen and (max-width: 64em) {
+    left: 10%;
+  }
+`;
+
+export const Couleur = styled.li`
+  list-style: none;
+  width: 1.5rem;
+  height: 1.5rem;
+  cursor: pointer;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0.5rem 0;
+  border: 1px solid var(--dark);
+`;
+export const Details = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color:yellow;
+`;
+
+export const Titre = styled.h2`
+  font-size: var(--fontxl);
+  padding: 0.3rem;
+`;
+
+export const SousTitre = styled.h2`
+  font-size: var(--fontmd);
+  font-family: var(--fontR);
+`;
+
+export const BtnContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 1rem;
+`;
+
+export const Btn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  padding: 0.4rem 1rem;
+  border-radius: 50px;
+  border: none;
+  outline: none;
+  background-color: var(--blue);
+  color: var(--white);
+  cursor: pointer;
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+export const BtnLink = styled.a`
+  color: var(--blue);
+  text-decoration: none;
+  margin-left: 1.5rem;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const Degree = styled.div`
+  font-size: var(--fontsm);
+  font-family: Georgia, 'Times New Roman', Times, serif;
+  position: absolute;
+  top: 2rem;
+`;
+export const P = styled.span`
+color:red;`
